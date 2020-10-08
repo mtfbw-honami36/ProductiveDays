e06389ada1ee8ef92a405139df7f9628eb005b05
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-10-08 17:33:05 +0900

    Todoの登録と表示

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,13 @@
 class TodosController < ApplicationController
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to root_path
+    end
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:name)
+  end
 end
