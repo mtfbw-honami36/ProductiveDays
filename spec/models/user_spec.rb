8c415cf994d92b067cc35f760e54f3864e239a50
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-10-14 16:45:01 +0900

    userモデルテスト実行

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it "nameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
+      expect(@user).to be_valid
+    end
+
+    it "nameが空では登録できないこと" do
+      @user.name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nameを入力してください")
+    end
+
+    it "emailが空では登録できないこと" do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("メールアドレスを入力してください")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("メールアドレスはすでに存在します")
+    end
+
+    it "passwordが空では登録できないこと" do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Passwordを入力してください")
+    end
+
+    it "passwordが5文字以下であれば登録できないこと" do
+      @user.password = "00000"
+      @user.password_confirmation = "00000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Passwordは6文字以上で入力してください")
+    end
+
+    it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmationとPasswordの入力が一致しません")
+    end
+  end
+end
