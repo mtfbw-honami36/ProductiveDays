5c2da82c1494b9438b7ce75b771a04c97230e5da
mtfbw-honami36 <honamigoo.9536@gmail.com>
2020-10-14 16:12:18 +0900

    scheduleモデルバリデーション追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,6 +5,6 @@ class Schedule < ApplicationRecord
   
   belongs_to :user
   
-  validates :name, :start_time, :countdown, :color, presence: true
+  validates :name, :start_time, :countdown, :color, :user_id, presence: true
   validates :countdown_id, :color_id, numericality: { other_than: 1 } 
 end
